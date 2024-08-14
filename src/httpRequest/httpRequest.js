5d2fca3d36515d7aac8e2c2aa6ea205232ebb7bc
allahverdiyevabdulhasan <info.neyman.e.t@gmail.com>
2024-08-14 18:58:21 +0400

    send

--- src/httpRequest/httpRequest.js
+++ src/httpRequest/httpRequest.js
@@ -1,4 +1,4 @@
-export const BASE_URL_EN="http://167.172.182.92/en"
-export const BASE_URL_TR="http://167.172.182.92/tr"
-export const BASE_URL_AZ="http://167.172.182.92/az"
-export const BASE_URL_RU="http://167.172.182.92/ru"
+export const BASE_URL_EN="http://neymantech.shop/en"
+export const BASE_URL_TR="http://neymantech.shop/tr"
+export const BASE_URL_AZ="http://neymantech.shop/az"
+export const BASE_URL_RU="http://neymantech.shop/ru"
