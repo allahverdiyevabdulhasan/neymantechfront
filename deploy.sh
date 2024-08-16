4663b937207bb57fd49d04b2a1c1a14b977603ee
allahverdiyevabdulhasan <info.neyman.e.t@gmail.com>
2024-08-17 00:26:55 +0400

    send

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ echo "Bulding app ..."
 npm run build
 
 echo "Deploying files to server..."
-scp -r dist/* neyman@134.209.237.235:/var/www/neymantech/
+scp -r dist/* neyman@134.209.237.235:/var/www/neymantech.com/
 echo "Done!"
